68205dcb1077797eea20293596ffa28e3f7192a9
kutub98 <kutubuddin98710@gmail.com>
2022-11-09 04:15:44 +0600

    gettingAllServices

--- index.js
+++ index.js
@@ -9,13 +9,49 @@ app.use(express.json())
 require('dotenv').config();
 
 
+// mongodb 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 app.get('/', (req, res)=>{
     res.send('legal aid is ready to run')
 })
 
 
-console.log(process.env.SECRET_KEY_OF_lEGAL_AID)
-console.log(process.env.SITE_NAME_IS_lEGAl_AID)
+
+const uri = `mongodb+srv://${process.env.SITE_NAME}:${process.env.SECRET_KEY_OF_lEGAL_AID}@cluster0.mlxcjcs.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+const allLegalServices = client.db("legalAid").collection("legalServices");
+async function run(){
+    try{
+        await client.connect();
+        console.log('Mongo has connected')
+    }catch (error) {
+        console.log(error.name, error.message);
+      }
+}
+
+// getting all Services
+app.get('/services', async(req, res )=>{
+    const query = {}
+    const cursor = allLegalServices.find(query);
+    const services = await cursor.toArray();
+    // console.log(services)
+    res.send(services)
+});
+
+app.get('/services/:id', async(req, res)=>{
+    const id = req.params.id;
+    const query = {_id: ObjectId(id)}
+    const getServiceByID= await allLegalServices.findOne(query);
+    console.log(getServiceByID)
+    res.send(getServiceByID)
+})
+
+run().catch(error => console.error(error))
+
+
 
 
 
